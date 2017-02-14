6033f14bd10d2463834a40191d6db697f6869237
Kevin Finley and Marty Kovach <marty@>
2017-02-14 09:05:13 -0800

    add passing validation spec for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :user_name, :presence => true
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :password_digest, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe User, type: :model do
   it { should have_db_column :last_name}
   it { should have_db_column :email}
   it { should have_db_column :password_digest}
+  it { should validate_presence_of :user_name}
+  it { should validate_presence_of :first_name}
+  it { should validate_presence_of :last_name}
+  it { should validate_presence_of :email}
+  it { should validate_presence_of :password_digest}
 end
