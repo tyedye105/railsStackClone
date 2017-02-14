e33f28167c92dc871d82b6bbba2e35938418fc7e
Kevin Finley and Marty Kovach <kevin@>
2017-02-14 10:45:31 -0800

    check for the columns of posts

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it { should have_db_column :user_id}
+  it { should have_db_column :content}
+  it { should have_db_column :created_at}
+  it { should have_db_column :updated_at}
+end
