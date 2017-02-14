717e7d822576efcbaee3961dee85e0a5d432b9b0
Kevin Finley and Marty Kovach <kevin@>
2017-02-14 10:04:16 -0800

    allow the user to signup, logout, login and add simplecov

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
   def create
     @user = User.find_by_user_name(params[:session][:user_name])
     if @user && @user.authenticate(params[:session][:password])
@@ -10,6 +12,7 @@ class SessionsController < ApplicationController
       redirect_to login_path
     end
   end
+  
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-
   validates :user_name, :presence => true
   validates :first_name, :presence => true
   validates :last_name, :presence => true
