5e25c7e3e516023b77a2028f8ac8a3a87c22850b
Kevin Finley and Marty Kovach <kevin@>
2017-02-14 10:47:02 -0800

    validate presence of content for posts.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Post, type: :model do
   it { should have_db_column :content}
   it { should have_db_column :created_at}
   it { should have_db_column :updated_at}
+  it { should validate_presence_of :content}
 end
