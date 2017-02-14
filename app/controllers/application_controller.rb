9b878820f66c306027b8b1bd5375c4d1e64bc80c
Kevin Finley and Marty Kovach <kevin@>
2017-02-14 09:44:44 -0800

    allow user to logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    if User.all == []
+      if @current_user != nil
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      else
+        nil
+      end
+    else
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+
+  def require_user
+    redirect_to '/login' unless current_user
+  end
+
 end
